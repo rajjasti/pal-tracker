9c7894a23cb789bc93a3e2d252e2c87632459547
Raj Jasti <raj.jasti@gmail.com>
2019-03-05 15:40:22 -0700

    Added JdbcTemplate and connected to DB

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -5,10 +5,13 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -17,9 +20,10 @@ public class PalTrackerApplication {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
+
     @Bean
-    public TimeEntryRepository createTimeEntryRepository(){
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository createTimeEntryRepository(DataSource source){
+        return new JdbcTimeEntryRepository(source);
     }
 
     @Bean
